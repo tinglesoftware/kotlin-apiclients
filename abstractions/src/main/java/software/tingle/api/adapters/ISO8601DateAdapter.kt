42e4b7675bfbbe5dce95822b2262f63d90124515
Seth Onyango <sethi.onyango@gmail.com>
2024-05-21 10:04:14 +0300

    Refactor ISO8601DateAdapter class to remove unnecessary inheritance and improve readability

--- abstractions/src/main/java/software/tingle/api/adapters/ISO8601DateAdapter.kt
+++ abstractions/src/main/java/software/tingle/api/adapters/ISO8601DateAdapter.kt
@@ -1,23 +1,32 @@
 package software.tingle.api.adapters
 
-import com.google.gson.*
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
 import com.google.gson.internal.bind.util.ISO8601Utils
 import java.lang.reflect.Type
 import java.text.ParseException
 import java.text.ParsePosition
-import java.util.*
+import java.util.Date
 
 /**
  * A @[JsonDeserializer] that (de)serializes a @[Date] in ISO8601.
  * Internally it utilizes @[ISO8601Utils] which is ported from Jackson databind
  */
 
-class ISO8601DateAdapter : ISO8601Utils(), JsonDeserializer<Date>, JsonSerializer<Date> {
+class ISO8601DateAdapter : JsonDeserializer<Date>, JsonSerializer<Date> {
 
     @Synchronized
-    override fun deserialize(jsonElement: JsonElement, type: Type, jsonDeserializationContext: JsonDeserializationContext): Date? {
+    override fun deserialize(
+        jsonElement: JsonElement,
+        type: Type,
+        jsonDeserializationContext: JsonDeserializationContext
+    ): Date? {
         return try {
-            parse(jsonElement.asString, ParsePosition(0))
+            ISO8601Utils.parse(jsonElement.asString, ParsePosition(0))
         } catch (e: ParseException) {
             e.printStackTrace()
             null
@@ -25,7 +34,11 @@ class ISO8601DateAdapter : ISO8601Utils(), JsonDeserializer<Date>, JsonSerialize
 
     }
 
-    override fun serialize(date: Date, type: Type, jsonSerializationContext: JsonSerializationContext): JsonElement {
-        return JsonPrimitive(format(date))
+    override fun serialize(
+        date: Date,
+        type: Type,
+        jsonSerializationContext: JsonSerializationContext
+    ): JsonElement {
+        return JsonPrimitive(ISO8601Utils.format(date))
     }
 }
